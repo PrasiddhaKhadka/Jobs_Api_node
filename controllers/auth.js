a44e289994d8bfd66d7e5748c85029721897fbfc
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-17 15:26:58 +0545

    Add: Refactor the code for jwt token at schema level

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,17 @@
 const User = require("../models/auth-models")
 const { StatusCodes } = require("http-status-codes")
 const { BadRequest } = require("../errors")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
 
 const register = async(req,res)=>{
     const { name, email, password } = req.body
     const user = await User.create({ name, email, password })
-    const token = jwt.sign({ userId: user._id,name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d'})
+    const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({
         msg: "User created successfully",
+        user: {
+            name: user.name,
+            email: user.email
+        },
         token
     })
 }
