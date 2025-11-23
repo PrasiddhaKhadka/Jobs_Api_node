4c458a4c5574cc58c5ea984be6e31420dec58c21
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-23 09:57:30 +0545

    Add: Security Packages like cors, xss, helmet and rate_limiter

---
+++ Readme..md
@@ -0,0 +1,6 @@
+## Security
+
+- helmet
+- cors
+- xss-clean
+- express-rate-limit
--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 require('dotenv').config()
+
+//SECRURITY PACAKGES
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app  = express()
 
@@ -11,7 +18,21 @@ const authenticationMiddleware = require('./middlewares/authentication')
 // DATABASE CONNECTION
 const connectDb = require('./db/connection_db')
 
+// RATE LIMITER
+app.set('trust proxy',1);
+app.use(rateLimiter(
+    {
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max:100 //  Limit each IP to 100 request per windowMs
+    }
+));
+
 app.use(express.json())
+//USING THE SECURITY MIDDLEWARE IN HERE 
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 const authRouter = require('./routes/auth-routes')
 app.use('/api/v1/auth', authRouter)
